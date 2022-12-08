da892c3f7432b7c6bdce51d6dc5e137ed5936f30
stmayna <stmayna@gmail.com>
2022-12-08 12:06:51 +0700

    H2 Done

---
+++ jsPractice/HOproject1_2/main.js
@@ -0,0 +1,17 @@
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+let service1Speed = "0 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
+
+document.getElementById("service1Name").innerHTML = service1Name;
+document.getElementById("service2Name").innerHTML = service2Name;
+document.getElementById("service3Name").innerHTML = service3Name;
+document.getElementById("service4Name").innerHTML = service4Name;
+document.getElementById("service1Speed").innerHTML = service1Speed;
+document.getElementById("service2Speed").innerHTML = service2Speed;
+document.getElementById("service3Speed").innerHTML = service3Speed;
+document.getElementById("service4Speed").innerHTML = service4Speed;
---
+++ jsPractice/main.js
@@ -0,0 +1,8 @@
+// window.alert("Hello World");
+let fName = document.getElementById("firstName");
+document.getElementById("firstName").value = value;
+fName.value = value;
+document.write("<p>Plant Choices for <br/>");
+document.write(
+  "<a href='http://planthardiness.ars.usda.gov'> hardiness zones</a> 5a-6b</p>"
+);
