80b47670c0912057858386ad5cfa1789349aa919
stmayna <stmayna@gmail.com>
2022-12-08 12:07:15 +0700

    H1 Done

---
+++ jsPractice/HOproject1_1/main.js
@@ -0,0 +1,11 @@
+//create ordered list
+document.write("<ol>");
+document.write("<li>Reduce spending on non-necessities.</li>");
+document.write(
+  "<li>Use extra money to pay off debt, starting with highest-interest credit card.</li>"
+);
+document.write("<li>Continue paying off debts until you are debt free.</li>");
+document.write(
+  "<li>Put a fixed percent of your pay aside in savings every payday.</li>"
+);
+document.write("</ol");
