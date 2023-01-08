2905203bd14a92f6f656783fd0c2d7c4863a2fb3
stmayna <stmayna@gmail.com>
2023-01-08 17:01:38 +0700

    sorting example

---
+++ pythonDaily/sorting.py
@@ -0,0 +1,19 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([12, 6, 99, 5, 75, 1, 10, 77, 15, 8, 3, 25, 4, 9]))
